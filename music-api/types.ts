9df02b566b02de0d866c44764654b80766687bd9
Khrunev Denis <deniskhrunev@gmail.com>
2024-09-06 10:53:50 +0600

    Создана структура моделей для сущностей user и track_history(hw-83)

--- music-api/types.ts
+++ music-api/types.ts
@@ -1,3 +1,5 @@
+import {Model} from 'mongoose';
+
 export interface ArtistWithoutId {
   name: string;
   image: string | null;
@@ -16,3 +18,16 @@ export interface TrackWithoutId {
   name: string;
   duration: string;
 }
+
+export interface UserFields {
+  username: string;
+  password: string;
+  token: string;
+}
+
+export interface UserMethods {
+  checkPassword(password: string): Promise<boolean>;
+  generateToken(): void;
+}
+
+export type UserModel = Model<UserFields, {}, UserMethods>;
